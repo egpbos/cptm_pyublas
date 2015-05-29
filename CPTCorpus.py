d8d9fcc9652370d1a74da473d7a80e48b2f89099
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-05-29 18:08:22 +0200

    Update CPTCorpus to return both topic and opinion words
    The CPTCorpus class was updated to process text files with topic words
    on the first line and opinion words on the second line and return both
    sets of words simultaneously when get_texts is called.

--- CPTCorpus.py
+++ CPTCorpus.py
@@ -3,33 +3,56 @@
 import gensim
 import glob
 import codecs
+from itertools import izip
 
 
-class CPTCorpus(gensim.corpora.TextCorpus):
+class CorpusPerspective(gensim.corpora.TextCorpus):
+    def __init__(self, input, lineNumber=0):
+        self.lineNumber = lineNumber
+        super(CorpusPerspective, self).__init__(input)
+
     def get_texts(self):
         for txt in self.input:
             with codecs.open(txt, 'rb', 'utf8') as f:
                 lines = f.readlines()
-                topic_words = []
-                if len(lines) >= 1:
-                        topic_words = lines[0].split()
-                        #print topic_words
-                yield topic_words
+                words = []
+                if len(lines) >= (self.lineNumber+1):
+                        words = lines[self.lineNumber].split()
+                        #print words
+                yield words
+
+    def __len__(self):
+        return super(CorpusPerspective, self).__len__()
+
+
+class CPTCorpus():
+    def __init__(self, input):
+        # TODO: make sure there is a single dictionary for the topic part
+        # (when there are multiple perspectives)
+        self.topicCorpus = CorpusPerspective(input, 0)
+        self.opinionCorpus = CorpusPerspective(input, 1)
+        self.input = input
+
+    def __iter__(self):
+        for topic_words, opinion_words in izip(self.topicCorpus,
+                                               self.opinionCorpus):
+            yield {'topic': topic_words, 'opinion': opinion_words}
 
     def __len__(self):
         return len(self.input)
-
 
 if __name__ == '__main__':
     files = glob.glob('/home/jvdzwaan/data/dilipad/generated/*.txt')
     files.sort()
 
     corpus = CPTCorpus(files)
+    print len(corpus)
+    print len(corpus.topicCorpus)
     #print corpus.dictionary
     for doc in corpus:
         print doc
-        for w in doc:
-            print corpus.dictionary[w[0]]
+        #for w in doc:
+        #    print corpus.dictionary[w[0]]
         print '----------'
     #print len(corpus.dictionary)
     #a = [sum([f for w, f in doc]) for doc in corpus]
@@ -37,8 +60,12 @@ if __name__ == '__main__':
     #print sorted(a)
     #print max(a)
 
-    #for k, v in corpus.dictionary.iteritems():
-    #    print k, v
+    print 'topic words'
+    for k, v in corpus.topicCorpus.dictionary.iteritems():
+        print k, v
+    print '\nopinion words'
+    for k, v in corpus.opinionCorpus.dictionary.iteritems():
+        print k, v
     #b = corpus.dictionary.keys()
     #b.sort()
     #print b
