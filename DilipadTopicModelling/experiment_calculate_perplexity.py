0ab925dfb3ccf135557a25379c0f4b3fb5307e03
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-07-28 15:03:00 +0200

    Fix parallelization (again)
    The calculate perplexity script now uses appy_async instead of apply.
    This means perplexity calculation is now truly parallelized.

--- DilipadTopicModelling/experiment_calculate_perplexity.py
+++ DilipadTopicModelling/experiment_calculate_perplexity.py
@@ -43,11 +43,15 @@ topic_perp = pd.DataFrame(columns=nTopics, index=nPerplexity)
 opinion_perp = pd.DataFrame(columns=nTopics, index=nPerplexity)
 
 pool = Pool(processes=4)
-results = [pool.apply(calculate_perplexity, args=(corpus, n, nIter, beta,
-                                                  out_dir, nPerplexity))
+results = [pool.apply_async(calculate_perplexity, args=(corpus, n, nIter, beta,
+                                                        out_dir, nPerplexity))
            for n in nTopics]
+pool.close()
+pool.join()
 
-for result in results:
+data = [p.get() for p in results]
+
+for result in data:
     for n, s, tw_perp, ow_perp in result:
         topic_perp.set_value(s, n, tw_perp)
         opinion_perp.set_value(s, n, ow_perp)
