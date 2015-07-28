0dc29df1e97b8c5f36320b55c659c8290f021c69
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-07-28 16:10:34 +0200

    Fix parallelization of number of topics script

--- DilipadTopicModelling/experiment_number_of_topics.py
+++ DilipadTopicModelling/experiment_number_of_topics.py
@@ -1,6 +1,6 @@
 import logging
 import glob
-from multiprocessing import Process
+from multiprocessing import Pool
 
 from CPTCorpus import CPTCorpus
 from CPT_Gibbs import GibbsSampler
@@ -23,18 +23,18 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 #logger.setLevel(logging.INFO)
 
-files = glob.glob('/home/jvdzwaan/data/tmp/test/*')
+files = glob.glob('/home/jvdzwaan/data/dilipad/20112012/gov_opp/*')
 
-out_dir = '/home/jvdzwaan/data/tmp/generated/test_exp/{}'
+out_dir = '/home/jvdzwaan/data/dilipad/res_20112012/{}'
 
 corpus = CPTCorpus(files, testSplit=20)
 corpus.filter_dictionaries(minFreq=5, removeTopTF=100, removeTopDF=100)
 corpus.save_dictionaries(directory=out_dir.format(''))
 corpus.save(out_dir.format('corpus.json'))
 
-#corpus = CPTCorpus.CPTCorpus.load('{}corpus.json'.format(out_dir),
-#                                  topicDict='{}/topicDict.dict'.format(out_dir),
-#                                  opinionDict='{}/opinionDict.dict'.format(out_dir))
+#corpus = CPTCorpus.load(out_dir.format('corpus.json'),
+#                        topicDict=out_dir.format('topicDict.dict'),
+#                        opinionDict=out_dir.format('opinionDict.dict'))
 
 nIter = 200
 beta = 0.02
@@ -42,14 +42,9 @@ beta = 0.02
 nTopics = range(20, 201, 20)
 logger.info('running Gibbs sampler for {} configurations'.format(len(nTopics)))
 
-processes = [Process(target=run_sampler,
-                     args=(corpus, n, nIter, beta, out_dir))
-             for n in nTopics]
-
-# Run processes
-for p in processes:
-    p.start()
-
-# Exit the completed processes
-for p in processes:
-    p.join()
+pool = Pool(processes=3)
+results = [pool.apply_async(run_sampler, args=(corpus, n, nIter, beta,
+                                               out_dir))
+           for n in nTopics]
+pool.close()
+pool.join()
