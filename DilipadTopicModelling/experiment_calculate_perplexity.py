2767877d7b3f702b143470fc450cf4391e6ed8db
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-07-31 12:58:27 +0200

    Final version of calculate perplexity script
    Mostly did some clean up.

--- DilipadTopicModelling/experiment_calculate_perplexity.py
+++ DilipadTopicModelling/experiment_calculate_perplexity.py
@@ -1,39 +1,27 @@
 import pandas as pd
 import logging
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool
 
 from CPTCorpus import CPTCorpus
 from CPT_Gibbs import GibbsSampler
 
 
-def calculate_perplexity(nTopics, nIter, beta, out_dir, nPerplexity):
-    try:
-        logger.info('started perplexity calculation for {} topics'.format(nTopics))
-        data_dir = '/scratch/users/jvdzwaan/data/exp_num_topics/'
-        corpus = CPTCorpus.load('{}corpus.json'.format(data_dir),
-                                topicDict='{}/topicDict.dict'.format(data_dir),
-                                opinionDict='{}/opinionDict.dict'.format(data_dir))
-        alpha = 50.0/nTopics
-        logger.info('running Gibbs sampler (nTopics: {}, nIter: {}, alpha: {}, '
-                    'beta: {})'.format(nTopics, nIter, alpha, beta))
-        sampler = GibbsSampler(corpus, nTopics=nTopics, nIter=nIter,
-                               alpha=alpha, beta=beta, beta_o=beta,
-                               out_dir=out_dir.format(nTopics))
-        sampler._initialize()
-        #sampler.run()
-        results = []
-        for s in nPerplexity:
-            logger.info('doing perplexity calculation ({}, {})'.format(nTopics, s))
-            tw_perp, ow_perp = sampler.perplexity(index=s)
-            #tw_perp, ow_perp = 2.34639912, 9.26547634 
-            results.append((nTopics, s, tw_perp, ow_perp))
-        logger.info('finished perplexity calculation for {} topics'.
-                    format(nTopics))
-        return results
-    except Exception as e:
-	logger.info('error')
-	logger.info(str(e))
-	return None
+def calculate_perplexity(corpus, nTopics, nIter, beta, out_dir, nPerplexity):
+    alpha = 50.0/nTopics
+    sampler = GibbsSampler(corpus, nTopics=nTopics, nIter=nIter,
+                           alpha=alpha, beta=beta, beta_o=beta,
+                           out_dir=out_dir.format(nTopics))
+    sampler._initialize()
+    #sampler.run()
+    results = []
+    print nPerplexity
+    for s in nPerplexity:
+        logger.info('doing perplexity calculation ({}, {})'.format(nTopics, s))
+        tw_perp, ow_perp = sampler.perplexity(index=s)
+        results.append((nTopics, s, tw_perp, ow_perp))
+    logger.info('finished perplexity calculation for {} topics'.
+                format(nTopics))
+    return results
 
 
 logger = logging.getLogger(__name__)
@@ -43,48 +31,38 @@ logging.getLogger('gensim').setLevel(logging.ERROR)
 logging.getLogger('CPTCorpus').setLevel(logging.ERROR)
 logging.getLogger('CPT_Gibbs').setLevel(logging.ERROR)
 
-print '# CPUs', cpu_count() 
-
 # load corpus
-#data_dir = '/scratch/users/jvdzwaan/data/exp_num_topics/'
-#corpus = CPTCorpus.load('{}corpus.json'.format(data_dir))
-#corpus = CPTCorpus.load('{}corpus.json'.format(data_dir),
-#                        topicDict='{}/topicDict.dict'.format(data_dir),
-#                        opinionDict='{}/opinionDict.dict'.format(data_dir))
+data_dir = '/home/jvdzwaan/data/tmp/generated/test_exp/'
+corpus = CPTCorpus.load('{}corpus.json'.format(data_dir),
+                        topicDict='{}/topicDict.dict'.format(data_dir),
+                        opinionDict='{}/opinionDict.dict'.format(data_dir))
 
 nIter = 200
 beta = 0.02
-out_dir = '/scratch/users/jvdzwaan/data/exp_num_topics/{}'
+out_dir = '/home/jvdzwaan/data/tmp/generated/test_exp/{}'
 
 nTopics = range(20, nIter+1, 20)
 nPerplexity = range(0, nIter+1, 10)
 
-pool = Pool(processes=1)
-results = [pool.apply_async(calculate_perplexity, args=(n, nIter, beta,
+# calculate perplexity
+pool = Pool(processes=5)
+results = [pool.apply_async(calculate_perplexity, args=(corpus, n, nIter, beta,
                             out_dir, nPerplexity))
+           # reverse list, so longest calculation is started first
            for n in nTopics[::-1]]
-logger.info('finished initialisation')
 pool.close()
-logger.info('called pool.close()')
 pool.join()
-logger.info('called pool.join()')
 
+# aggrate and save results
 data = [p.get() for p in results]
-logger.info('created data')
 
 topic_perp = pd.DataFrame(columns=nTopics, index=nPerplexity)
 opinion_perp = pd.DataFrame(columns=nTopics, index=nPerplexity)
 
 for result in data:
-    if result is None:
-        logger.error('None result')
-    else:
-        for n, s, tw_perp, ow_perp in result:
-            topic_perp.set_value(s, n, tw_perp)
-            opinion_perp.set_value(s, n, ow_perp)
-
-            #logger.info('nTopics: {}, nPerplexity: {}, topic perplexity: {}, '
-            #            'opinion perplexity: {}'.format(n, s, tw_perp, ow_perp))
+    for n, s, tw_perp, ow_perp in result:
+        topic_perp.set_value(s, n, tw_perp)
+        opinion_perp.set_value(s, n, ow_perp)
 
 topic_perp.to_csv(out_dir.format('perplexity_topic.csv'))
 opinion_perp.to_csv(out_dir.format('perplexity_opinion.csv'))
