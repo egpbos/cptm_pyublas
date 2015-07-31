eee0d3594ea6da8553b13682e362ab14172c9a38
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-07-31 17:04:53 +0200

    Move generic experiment functionality to utils module
    Added an utils module and moved generic functionality for experiments to
    this new module. Also updated the experiment_number_of_topics script.

--- DilipadTopicModelling/experiment_number_of_topics.py
+++ DilipadTopicModelling/experiment_number_of_topics.py
@@ -1,22 +1,12 @@
 import logging
-import glob
 from multiprocessing import Pool
 import argparse
-import json
 
-from CPTCorpus import CPTCorpus
-from CPT_Gibbs import GibbsSampler
+from utils.experiment import load_config, get_corpus, get_sampler
 
 
-def run_sampler(corpus, nTopics, nIter, beta, beta_o, out_dir):
-    alpha = 50.0/nTopics
-    logger.info('running Gibbs sampler (nTopics: {}, nIter: {}, alpha: {}, '
-                'beta: {}, beta_o: {})'.format(nTopics, nIter, alpha, beta,
-                                               beta_o))
-    sampler = GibbsSampler(corpus, nTopics=nTopics, nIter=nIter,
-                           alpha=alpha, beta=beta, beta_o=beta_o,
-                           out_dir=out_dir.format(nTopics))
-    sampler._initialize()
+def run_sampler(config, corpus, nTopics):
+    sampler = get_sampler(config, corpus, nTopics)
     sampler.run()
 
 
@@ -28,44 +18,14 @@ parser.add_argument('json', help='json file containing experiment '
                     'configuration.')
 args = parser.parse_args()
 
-with open(args.json) as f:
-    config = json.load(f)
+config = load_config(args.json)
+corpus = get_corpus(config)
 
-logger.debug('configuration of experiment: ')
-params = ['{}: {}'.format(p, v) for p, v in config.iteritems()]
-for p in params:
-    logger.debug(p)
-
-files = glob.glob(config.get('input_data'))
-
-out_dir = config.get('out_dir', '/{}')
-testSplit = config.get('testSplit', 20)
-minFreq = config.get('minFreq', 5)
-removeTopTF = config.get('removeTopTF', 100)
-removeTopDF = config.get('removeTopDF', 100)
-nIter = config.get('nIter', 200)
-beta = config.get('beta', 0.02)
-beta_o = config.get('beta_o', 0.02)
-nTopics = config.get('nTopics', range(20, 201, 20))
-nProcesses = config.get('nProcesses', None)
-loadDictionaries = config.get('loadDictionaries', 0)
-
-if not loadDictionaries:
-    corpus = CPTCorpus(files, testSplit=testSplit)
-    corpus.filter_dictionaries(minFreq=minFreq, removeTopTF=removeTopTF,
-                               removeTopDF=removeTopDF)
-    corpus.save_dictionaries(directory=out_dir.format(''))
-    corpus.save(out_dir.format('corpus.json'))
-else:
-    corpus = CPTCorpus.load(out_dir.format('corpus.json'),
-                            topicDict=out_dir.format('topicDict.dict'),
-                            opinionDict=out_dir.format('opinionDict.dict'))
-
+nTopics = config.get('nTopics')
 logger.info('running Gibbs sampler for {} configurations'.format(len(nTopics)))
 
-pool = Pool(processes=nProcesses)
-results = [pool.apply_async(run_sampler, args=(corpus, n, nIter, beta, beta_o,
-                                               out_dir))
+pool = Pool(processes=config.get('nProcesses'))
+results = [pool.apply_async(run_sampler, args=(config, corpus, n))
            for n in nTopics]
 pool.close()
 pool.join()
---
+++ DilipadTopicModelling/utils/experiment.py
@@ -0,0 +1,71 @@
+"""Functions for experiments."""
+import logging
+import json
+import glob
+import os
+
+from CPTCorpus import CPTCorpus
+from CPT_Gibbs import GibbsSampler
+
+logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def load_config(fName):
+    with open(fName) as f:
+        config = json.load(f)
+
+    logger.debug('configuration of experiment: ')
+    params = ['{}: {}'.format(p, v) for p, v in config.iteritems()]
+    for p in params:
+        logger.debug(p)
+
+    params = {}
+    params['inputData'] = config.get('input_data')
+    params['outDir'] = config.get('out_dir', '/{}')
+    params['testSplit'] = config.get('testSplit', 20)
+    params['minFreq'] = config.get('minFreq', 5)
+    params['removeTopTF'] = config.get('removeTopTF', 100)
+    params['removeTopDF'] = config.get('removeTopDF', 100)
+    params['nIter'] = config.get('nIter', 200)
+    params['beta'] = config.get('beta', 0.02)
+    params['beta_o'] = config.get('beta_o', 0.02)
+    params['nTopics'] = config.get('nTopics', range(20, 201, 20))
+    params['nProcesses'] = config.get('nProcesses', None)
+
+    return params
+
+
+def get_corpus(params):
+    out_dir = params.get('outDir')
+    files = glob.glob(params.get('input_data'))
+
+    if not os.path.isfile(os.path.join(out_dir, 'corpus.json')):
+        corpus = CPTCorpus(files,
+                           testSplit=params.get('testSplit'))
+        corpus.filter_dictionaries(minFreq=params.get('minFreq'),
+                                   removeTopTF=params.get('removeTopTF'),
+                                   removeTopDF=params.get('removeTopDF'))
+        corpus.save_dictionaries(directory=out_dir.format(''))
+        corpus.save(out_dir.format('corpus.json'))
+    else:
+        corpus = CPTCorpus.load(out_dir.format('corpus.json'),
+                                topicDict=out_dir.format('topicDict.dict'),
+                                opinionDict=out_dir.format('opinionDict.dict'))
+    return corpus
+
+
+def get_sampler(params, corpus, nTopics):
+    out_dir = params.get('outDir')
+    nIter = params.get('nIter')
+    alpha = 50.0/nTopics
+    beta = params.get('beta')
+    beta_o = params.get('beta_o')
+    logger.info('running Gibbs sampler (nTopics: {}, nIter: {}, alpha: {}, '
+                'beta: {}, beta_o: {})'.format(nTopics, nIter, alpha, beta,
+                                               beta_o))
+    sampler = GibbsSampler(corpus, nTopics=nTopics, nIter=nIter,
+                           alpha=alpha, beta=beta, beta_o=beta_o,
+                           out_dir=out_dir.format(nTopics))
+    sampler._initialize()
+    return sampler
