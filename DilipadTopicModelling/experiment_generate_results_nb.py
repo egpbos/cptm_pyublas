b9d67e011db36db8671108c4c56eaf8f5256a24e
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-07 15:08:22 +0200

    Update generated ipython notebook
    Removed nTopics as argument (this number is chosen based on the
    perplexity results and should therefore not be a parameter of generating
    the notebook).
    Also made some other small improvements to the notebook.

--- DilipadTopicModelling/experiment_generate_results_nb.py
+++ DilipadTopicModelling/experiment_generate_results_nb.py
@@ -10,17 +10,14 @@ logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser()
 parser.add_argument('resultsDir', help='directory containing experiment'
                     'result files.')
-parser.add_argument('experimentName', help='name of the experiment (used as '
-                    'file name for output).')
-parser.add_argument('nTopics', help='the number of topics to print results '
-                    'for.')
-parser.add_argument('outDir', help='directory to write IPython Notebook to')
+parser.add_argument('experimentName', help='name of the experiment')
+parser.add_argument('outFile', help='file name to write IPython Notebook to '
+                    '(*.ipynb)')
 args = parser.parse_args()
 
 resultsDir = args.resultsDir
 experimentName = args.experimentName
-nTopics = args.nTopics
-outDir = args.outDir
+outFile = args.outFile
 
 with open('data/CPT_results_template.ipynb') as f:
     nb = nbf.read(f, 4)
@@ -29,15 +26,8 @@ with open('data/CPT_results_template.ipynb') as f:
 nb['cells'][0]['source'] = nb['cells'][0]['source'].format(experimentName)
 # cell 4 = set results dir
 nb['cells'][4]['source'] = nb['cells'][4]['source'].format(resultsDir)
-# cell 11 = set nTopics
-nb['cells'][11]['source'] = nb['cells'][11]['source'].format(nTopics)
-# cell 13 = set nTopics
-nb['cells'][13]['source'] = nb['cells'][13]['source'].format(nTopics)
-# cell 22 = set nTopics
-nb['cells'][22]['source'] = nb['cells'][22]['source'].format(nTopics)
 
 # save notebook
-fName = '{}/{}_{}.ipynb'.format(outDir, experimentName, nTopics)
-logger.info('writing notebook {}'.format(fName))
-with codecs.open(fName, 'wb', encoding='utf8') as f:
+logger.info('writing notebook {}'.format(outFile))
+with codecs.open(outFile, 'wb', encoding='utf8') as f:
     nbf.write(nb, f)
