3b572976346b455073359661b1e3ac7f0dd21f13
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-10 23:39:37 +0200

    Update setup.py with new name and argument for mac
    setup.py was updated with the new directory name (cptm instead of
    DilipadTopicModelling) and added an argument that is required on the mac
    for compling the cython code.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy as np
 
 setup(
-    ext_modules = cythonize("DilipadTopicModelling/gibbs_inner.pyx")
+    ext_modules = cythonize("cptm/gibbs_inner.pyx"),
+    include_dirs=[np.get_include()]
 )
